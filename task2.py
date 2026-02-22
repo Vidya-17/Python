ad73bf39d1343eca902ebb794ea7bbed615c8bd7
Vidya-17 <vidyaprathi17@gmail.com>
2026-02-22 09:31:38 +0530

    Find if the number is even or odd

---
+++ task2.py
@@ -0,0 +1,9 @@
+nums =[1,2,3,4,5,6,7,8,9,10]
+def evod(nums):
+    for num in nums:
+        if (num%2==0):
+            print (num, 'is even')
+        else:
+            print (num, 'is odd')
+
+evod(nums)
